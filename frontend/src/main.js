2e0d5153823cdcae0e6eb09896a5b6b5ee7ff90f
Nicolai Brandt <brandt.nicolai@gmail.com>
2020-03-15 23:04:33 +0100

    add bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
 import App from "./App.vue";
 import router from "./router";
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
---
+++ frontend/src/views/SpeedTest.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="speedtest">
+    <h1>Speedtest</h1>
+    <h2>Status: <b-badge :variant="enabledvariant">{{ is_enabled ? "enabled": "disabled "}}</b-badge></h2>
+    <div>
+      <b-button-group>
+        <b-button @click="startSpeedTest()">Start</b-button>
+        <b-button @click="stopSpeedTest()">Stop</b-button>
+      </b-button-group>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SpeedTest",
+  data() {
+    return {
+      is_enabled: null,
+      enabledvariant: "secondary"
+    };
+  },
+  methods: {
+    startSpeedTest() {
+      this.axios
+        .post("http://127.0.0.1:5000/speedtest", {
+          job: "start",
+          minute: 2
+        })
+        .then(response => (this.is_enabled = response.data.is_enabled));
+    },
+    stopSpeedTest() {
+      this.axios
+        .post("http://127.0.0.1:5000/speedtest", {
+          job: "stop"
+        })
+        .then(response => (this.is_enabled = response.data.is_enabled));
+    }
+  },
+  watch: {
+    is_enabled: function(val) {
+      if (val) {
+        this.enabledvariant = "success";
+      } else {
+        this.enabledvariant = "danger";
+      }
+    }
+  },
+  mounted() {
+    this.axios
+      .get("http://127.0.0.1:5000/speedtest")
+      .then(response => (this.is_enabled = response.data.is_enabled));
+  }
+};
+</script>
