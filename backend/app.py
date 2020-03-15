688cdd3b0e12ff800811e908f75f38aa45df594c
Nicolai Brandt <brandt.nicolai@gmail.com>
2020-03-15 21:46:17 +0100

    initial

---
+++ backend/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+import os
+
+from crontab import CronTab
+
+app = Flask(__name__)
+api = Api(app)
+
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+class SpeedTest(Resource):
+    def __init__(self):
+        self.job = None
+        self.cron = CronTab(user=True)
+        command = os.environ["SPEEDTEST_CMD"]
+
+        for job in self.cron:
+            if job.comment == "speedtest":
+                self.job = job
+        if not self.job:
+            self.job = self.cron.new(command=command, comment="speedtest")
+            self.job.minute.every(2)
+            self.job.enable(False)
+            self.cron.write()
+
+    def get(self):
+        return {"is_enabled": self.job.is_enabled()}
+
+    def post(self):
+        data = request.get_json()
+        if data.get("job") == "start":
+            self.job.enable()
+            if data.get("minute"):
+                self.job.minute.every(data.get("minute"))
+            self.cron.write()
+            return {"is_enabled": True}
+        elif data.get("job") == "stop":
+            self.job.enable(False)
+            self.cron.write()
+            return {"is_enabled": False}
+        return False
+
+
+api.add_resource(SpeedTest, "/speedtest")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ backend/speedtest_job.py
@@ -0,0 +1,75 @@
+import speedtest
+from datetime import datetime
+import os
+from csv import DictWriter
+import pathlib
+
+
+BASE_DIR = pathlib.Path(__file__).parent.absolute()
+LOG_CSV = os.path.join(BASE_DIR, "connection_log.csv")
+
+
+class SpeedTest():
+    def __init__(self, servers=[], threads=None):
+        self.servers = servers
+        self.threads = threads
+        self.time = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        self.result_json = None
+
+    def run(self):
+        try:
+            self.st = speedtest.Speedtest()
+        except speedtest.ConfigRetrievalError or speedtest.ShareResultsConnectFailure:
+            self.no_connection()
+            return
+
+        self.st.get_servers(self.servers)
+        self.st.get_best_server()
+        self.st.download(threads=self.threads)
+        self.st.upload(threads=self.threads)
+        self.st.results.share()
+
+        result = self.st.results.dict()
+
+        self.result_json = {
+            "time": self.time,
+            "connected": True,
+            "download": result["download"],
+            "upload": result["upload"],
+            "ping": result["ping"],
+            "server": result["server"]["url"]
+        }
+
+    def no_connection(self):
+        self.result_json = {
+            "time": self.time,
+            "connected": False,
+            "download": 0.0,
+            "upload": 0.0,
+            "ping": 0.0,
+            "server": "",
+        }
+
+    def write_log(self):
+
+        csv_exists = os.path.isfile(LOG_CSV)
+
+        with open(LOG_CSV, "a") as csvfile:
+            fieldnames = ["time", "connected", "download", "upload", "ping", "server"]
+            writer = DictWriter(csvfile, fieldnames)
+            if not csv_exists:
+                writer.writeheader()
+            writer.writerow(self.result_json)
+
+
+if __name__ == "__main__":
+    print("Initializing ...")
+    speed_test = SpeedTest()
+
+    print("Running ...")
+    speed_test.run()
+
+    print("Writing log file ...")
+    speed_test.write_log()
+
+    print("Done!")
