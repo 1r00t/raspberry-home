d5cbcc6ea82e4c917688a0f57342acffa2b2f6be
Nicolai Brandt <brandt.nicolai@gmail.com>
2020-03-17 10:12:52 +0100

    jsonify

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 # from flask_cors import CORS
 import os
@@ -26,7 +26,7 @@ class SpeedTest(Resource):
             self.cron.write()
 
     def get(self):
-        return {"is_enabled": self.job.is_enabled()}
+        return jsonify({"is_enabled": self.job.is_enabled()})
 
     def post(self):
         data = request.get_json()
@@ -35,21 +35,15 @@ class SpeedTest(Resource):
             if data.get("minute"):
                 self.job.minute.every(data.get("minute"))
             self.cron.write()
-            return {"is_enabled": True}
+            return jsonify({"is_enabled": True})
         elif data.get("job") == "stop":
             self.job.enable(False)
             self.cron.write()
-            return {"is_enabled": False}
+            return jsonify({"is_enabled": False})
         return False
 
 
 api.add_resource(SpeedTest, "/speedtest")
-
-
-@app.after_request
-def add_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
 
 
 if __name__ == "__main__":
